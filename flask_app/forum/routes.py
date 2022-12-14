75caf0c53a879f287a636b5eb915e760186276bc
hamzahs1 <84736257+hamzahs1@users.noreply.github.com>
2022-12-14 16:55:50 -0500

    Added question posting functionality to forum/routes.py

--- flask_app/forum/routes.py
+++ flask_app/forum/routes.py
@@ -1,12 +1,19 @@
-from flask import Blueprint, render_template, url_for, redirect
-from flask_login import current_user
+from flask import Blueprint, render_template, url_for, redirect, request, flash
+from flask_mail import Mail
+from flask_login import current_user, login_required, login_user, logout_user
+from flask_mongoengine import MongoEngine
 from .. import bcrypt
-from flask_app.forms import SearchForm
-from ..models import User
+from flask_app.forms import SearchForm, QuestionForm
+from ..models import User, Question, Answer
 from flask_app import db
+from datetime import datetime
+from .utils import current_time
 
 
 forum = Blueprint("forum", __name__)
+
+mail = Mail(forum)
+
 @forum.route("/")
 @forum.route("/forum")
 def index():
@@ -21,3 +28,28 @@ def search_results(query):
     except ValueError as e:
         return render_template("search_results.html", error_msg="No results found")
     return render_template("search_results.html", results=results)
+
+# TODO make sure the route is correct
+@forum.route("posts/<post_title>", methods = ["GET","POST"])
+@login_required
+def make_post():
+
+    # TODO make sure the below works
+    form = QuestionForm()
+    if form.validate_on_submit() and current_user.is_authenticated:
+        post= Question(
+            commenter=current_user._get_current_object(),
+            title=form.title.data,
+            description = form.description.data,
+            date = current_time()
+        )
+        post.save()
+
+        return redirect(request.path)
+
+    # TODO Figure out what questions should be...
+    questions = Question.objects(commenter = current_user._get_current_object())
+    
+    # TODO render appropriate template with corresponding data for it
+    return render_template(
+        "404.html", form=form)
