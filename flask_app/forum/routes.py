dcd41fa8c2c8c638e01632bace8a84bab75f8f90
MadsAndersens <s204137@student.dtu.dk>
2022-12-14 15:31:09 -0500

    Revert "Merge branch 'main' of https://github.com/MadsAndersens/CMSC388jFinalProject"
    This reverts commit 5997f5f8de83172bd9e4135dc186f08e818347f8, reversing
    changes made to 728a41c2434de2c3d8a880c2a08efb4ae6d323d9.

--- flask_app/forum/routes.py
+++ flask_app/forum/routes.py
@@ -2,24 +2,27 @@ from flask import Blueprint, redirect, url_for, render_template, flash, request
 from flask_login import current_user
 
 from .. import bcrypt
-from .forms import SearchForm
+from ..forms import QuestionForm, AnswerForm, SearchForm
 from ..models import User
-from flask_app import db
-
 
 forum = Blueprint("forum", __name__)
 
-
-@forum.route("/")
-@forum.route("/forum")
+@forum.route("/", methods=["GET", "POST"])
 def index():
     search_form = SearchForm()
 
-    if search_form.validate_on_submit():
-        return redirect(url_for("forum.search_results", query=search_form.search_query.data))
+    if form.validate_on_submit():
+        return redirect(url_for("forum.search_results", query=search_form.search.data))
 
-    return render_template("index.html", search_form=search_form)
+    return render_template("index.html", form=search_form)
 
-@forum.route("/search_results/<query>", methods=["GET", "POST"])
+@forum.route("/results/<query>", methods=["GET"])
 def search_results(query):
+    try: 
+        result = forum_client.search(query)
+    except ValueError as e:
+        flash(str(e))
+        return redirect(url_for("main.index"))
+    
+    return render_template("query.html")
     # TODO: Add search_results and html page
